0183be8d37f3f1d5c1f90968de0783640dd1f1c9
Kevin Waller <kevinwaller@ziggo.nl>
2022-04-19 16:15:18 +0200

    Random method now outputs classification labels rather than scores

--- ChangeDetection/models/random.py
+++ ChangeDetection/models/random.py
@@ -6,6 +6,6 @@ class RandomDetector(object):
         self.G1 = G1.copy()
     
     def forward(self, T2):
-        edge_scores = {edge: np.random.random() for edge in self.G1.edges}
+        edge_scores = {edge: np.random.choice(2) for edge in self.G1.edges}
         nx.set_edge_attributes(self.G1, edge_scores, name='weight')
         return self.G1
