8dd71fb126e9937349c80367cb01d1f6547031f5
Kevin Waller <waller@deepdriver03.flatns.net>
2022-05-06 16:04:06 +0200

    fixed mistake in last commit

--- ChangeDetection/batch_experiments.py
+++ ChangeDetection/batch_experiments.py
@@ -20,53 +20,53 @@ if __name__ == '__main__':
 
     args = parser.parse_args()
 
-    # starttime = datetime.now()
-    # starttime_experiments = starttime
-    # print(f'Starting Experiment 1 w/ No Noise - Start Time: {starttime.strftime("%H:%M:%S")}')
-    # os.system(f"python run_experiments.py --exp_name {args.exp_name}_seed{args.seed}_nonoise --results_dir results_{args.exp_name}_seed{args.seed} --dataset_dir {args.dataset_dir} \
-    #             --num_cpu_hmm {args.num_cpu_hmm} --map_index {args.map_index} --bbox {args.bbox[0]} {args.bbox[1]} {args.bbox[2]} {args.bbox[3]} --n_traj {args.n_traj} \
-    #                 --split_threshold {args.split_threshold} --seed {args.seed}")
-    # stoptime = datetime.now()
-    # delta = stoptime - starttime
-    # print(f'Experiment 1 Finished w/ No Noise - End Time: {stoptime.strftime("%H:%M:%S")}, Duration: {str(delta)}')
+    starttime = datetime.now()
+    starttime_experiments = starttime
+    print(f'Starting Experiment 1 w/ No Noise - Start Time: {starttime.strftime("%H:%M:%S")}')
+    os.system(f"python run_experiments.py --exp_name {args.exp_name}_seed{args.seed}_nonoise --results_dir results_{args.exp_name}_seed{args.seed} --dataset_dir {args.dataset_dir} \
+                --num_cpu_hmm {args.num_cpu_hmm} --map_index {args.map_index} --bbox {args.bbox[0]} {args.bbox[1]} {args.bbox[2]} {args.bbox[3]} --n_traj {args.n_traj} \
+                    --split_threshold {args.split_threshold} --seed {args.seed}")
+    stoptime = datetime.now()
+    delta = stoptime - starttime
+    print(f'Experiment 1 Finished w/ No Noise - End Time: {stoptime.strftime("%H:%M:%S")}, Duration: {str(delta)}')
 
-    # starttime = datetime.now()
-    # print(f'Starting Experiment 2 w/ Noise Config 0 - Start Time: {starttime.strftime("%H:%M:%S")}')
-    # os.system(f"python run_experiments.py --exp_name {args.exp_name}_seed{args.seed}_noiseconfig0 --noise --noise_config 0 --results_dir results_{args.exp_name}_seed{args.seed} --dataset_dir {args.dataset_dir} \
-    #             --num_cpu_hmm {args.num_cpu_hmm} --map_index {args.map_index} --bbox {args.bbox[0]} {args.bbox[1]} {args.bbox[2]} {args.bbox[3]} --n_traj {args.n_traj} \
-    #                 --split_threshold {args.split_threshold} --seed {args.seed}")
-    # stoptime = datetime.now()
-    # delta = stoptime - starttime
-    # print(f'Experiment 2 Finished w/ Noise Config 0 - End Time: {stoptime.strftime("%H:%M:%S")}, Duration: {str(delta)}')
+    starttime = datetime.now()
+    print(f'Starting Experiment 2 w/ Noise Config 0 - Start Time: {starttime.strftime("%H:%M:%S")}')
+    os.system(f"python run_experiments.py --exp_name {args.exp_name}_seed{args.seed}_noiseconfig0 --noise --noise_config 0 --results_dir results_{args.exp_name}_seed{args.seed} --dataset_dir {args.dataset_dir} \
+                --num_cpu_hmm {args.num_cpu_hmm} --map_index {args.map_index} --bbox {args.bbox[0]} {args.bbox[1]} {args.bbox[2]} {args.bbox[3]} --n_traj {args.n_traj} \
+                    --split_threshold {args.split_threshold} --seed {args.seed}")
+    stoptime = datetime.now()
+    delta = stoptime - starttime
+    print(f'Experiment 2 Finished w/ Noise Config 0 - End Time: {stoptime.strftime("%H:%M:%S")}, Duration: {str(delta)}')
     
-    # starttime = datetime.now()
-    # print(f'Starting Experiment 3 w/ Noise Config 1 - Start Time: {starttime.strftime("%H:%M:%S")}')
-    # os.system(f"python run_experiments.py --exp_name {args.exp_name}_seed{args.seed}_noiseconfig1 --noise --noise_config 1 --results_dir results_{args.exp_name}_seed{args.seed} --dataset_dir {args.dataset_dir} \
-    #             --num_cpu_hmm {args.num_cpu_hmm} --map_index {args.map_index} --bbox {args.bbox[0]} {args.bbox[1]} {args.bbox[2]} {args.bbox[3]} --n_traj {args.n_traj} \
-    #                 --split_threshold {args.split_threshold} --seed {args.seed}")
-    # stoptime = datetime.now()
-    # delta = stoptime - starttime
-    # print(f'Experiment 3 Finished w/ Noise Config 1 - End Time: {stoptime.strftime("%H:%M:%S")}, Duration: {str(delta)}')
+    starttime = datetime.now()
+    print(f'Starting Experiment 3 w/ Noise Config 1 - Start Time: {starttime.strftime("%H:%M:%S")}')
+    os.system(f"python run_experiments.py --exp_name {args.exp_name}_seed{args.seed}_noiseconfig1 --noise --noise_config 1 --results_dir results_{args.exp_name}_seed{args.seed} --dataset_dir {args.dataset_dir} \
+                --num_cpu_hmm {args.num_cpu_hmm} --map_index {args.map_index} --bbox {args.bbox[0]} {args.bbox[1]} {args.bbox[2]} {args.bbox[3]} --n_traj {args.n_traj} \
+                    --split_threshold {args.split_threshold} --seed {args.seed}")
+    stoptime = datetime.now()
+    delta = stoptime - starttime
+    print(f'Experiment 3 Finished w/ Noise Config 1 - End Time: {stoptime.strftime("%H:%M:%S")}, Duration: {str(delta)}')
     
-    # starttime = datetime.now()
-    # print(f'Starting Experiment 4 w/ Noise Config 2 - Start Time: {starttime.strftime("%H:%M:%S")}')
-    # os.system(f"python run_experiments.py --exp_name {args.exp_name}_seed{args.seed}_noiseconfig2 --noise --noise_config 2 --results_dir results_{args.exp_name}_seed{args.seed} --dataset_dir {args.dataset_dir} \
-    #             --num_cpu_hmm {args.num_cpu_hmm} --map_index {args.map_index} --bbox {args.bbox[0]} {args.bbox[1]} {args.bbox[2]} {args.bbox[3]} --n_traj {args.n_traj} \
-    #                 --split_threshold {args.split_threshold} --seed {args.seed}")
-    # stoptime = datetime.now()
-    # delta = stoptime - starttime
-    # print(f'Experiment 4 Finished w/ Noise Config 2 - End Time: {stoptime.strftime("%H:%M:%S")}, Duration: {str(delta)}')
+    starttime = datetime.now()
+    print(f'Starting Experiment 4 w/ Noise Config 2 - Start Time: {starttime.strftime("%H:%M:%S")}')
+    os.system(f"python run_experiments.py --exp_name {args.exp_name}_seed{args.seed}_noiseconfig2 --noise --noise_config 2 --results_dir results_{args.exp_name}_seed{args.seed} --dataset_dir {args.dataset_dir} \
+                --num_cpu_hmm {args.num_cpu_hmm} --map_index {args.map_index} --bbox {args.bbox[0]} {args.bbox[1]} {args.bbox[2]} {args.bbox[3]} --n_traj {args.n_traj} \
+                    --split_threshold {args.split_threshold} --seed {args.seed}")
+    stoptime = datetime.now()
+    delta = stoptime - starttime
+    print(f'Experiment 4 Finished w/ Noise Config 2 - End Time: {stoptime.strftime("%H:%M:%S")}, Duration: {str(delta)}')
     
-    # starttime = datetime.now()
-    # print(f'Starting Experiment 5 w/ Noise Config 3 - Start Time: {starttime.strftime("%H:%M:%S")}')
-    # os.system(f"python run_experiments.py --exp_name {args.exp_name}_seed{args.seed}_noiseconfig3 --noise --noise_config 3 --results_dir results_{args.exp_name}_seed{args.seed} --dataset_dir {args.dataset_dir} \
-    #             --num_cpu_hmm {args.num_cpu_hmm} --map_index {args.map_index} --bbox {args.bbox[0]} {args.bbox[1]} {args.bbox[2]} {args.bbox[3]} --n_traj {args.n_traj} \
-    #                 --split_threshold {args.split_threshold} --seed {args.seed}")
-    # stoptime = datetime.now()
-    # delta = stoptime - starttime
-    # print(f'Experiment 5 Finished w/ Noise Config 3 - End Time: {stoptime.strftime("%H:%M:%S")}, Duration: {str(delta)}')
-    # delta = stoptime - starttime_experiments
-    # print(f'All experiments finished at {stoptime.strftime("%H:%M:%S")}. Total duration: {str(delta)}')
+    starttime = datetime.now()
+    print(f'Starting Experiment 5 w/ Noise Config 3 - Start Time: {starttime.strftime("%H:%M:%S")}')
+    os.system(f"python run_experiments.py --exp_name {args.exp_name}_seed{args.seed}_noiseconfig3 --noise --noise_config 3 --results_dir results_{args.exp_name}_seed{args.seed} --dataset_dir {args.dataset_dir} \
+                --num_cpu_hmm {args.num_cpu_hmm} --map_index {args.map_index} --bbox {args.bbox[0]} {args.bbox[1]} {args.bbox[2]} {args.bbox[3]} --n_traj {args.n_traj} \
+                    --split_threshold {args.split_threshold} --seed {args.seed}")
+    stoptime = datetime.now()
+    delta = stoptime - starttime
+    print(f'Experiment 5 Finished w/ Noise Config 3 - End Time: {stoptime.strftime("%H:%M:%S")}, Duration: {str(delta)}')
+    delta = stoptime - starttime_experiments
+    print(f'All experiments finished at {stoptime.strftime("%H:%M:%S")}. Total duration: {str(delta)}')
 
     fscore_vs_noise(folder=f'./experimental_results/results_{args.exp_name}_seed{args.seed}/', savename=f'./experimental_results/results_{args.exp_name}_seed{args.seed}/fscore_vs_noise')
     plt.close()
