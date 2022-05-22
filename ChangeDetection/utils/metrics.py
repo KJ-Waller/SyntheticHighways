744e04f2d3a28d39a38b62dddf5c8c366e225024
Kevin Waller <waller@deepdriver03.flatns.net>
2022-05-22 16:00:33 +0200

    Changed plotting style in scripts and updated the tfreq experiment plots accordingly

--- ChangeDetection/utils/metrics.py
+++ ChangeDetection/utils/metrics.py
@@ -45,13 +45,16 @@ def predicted_labels(G_pred):
     sorted_scores = {edge: scores[edge] for edge in sorted(scores)}
     return sorted_scores
     
-def PRCurve(gt_labels, pred_scores, log_scale=True, norm=False, savename=None):
+def PRCurve(gt_labels, pred_scores, log_scale=True, norm=False, savename=None, figsize=(8,6)):
     """
     Plots the precision recall curve given ground truth labels and predicted scores
     """
     plt.clf()
+
+    # Make sure labels are aligned according to order of edges in gt_labels
     predictions = {k: np.float64(pred_scores[k]).item() for k in gt_labels}
 
+    # Plot log scale if specified
     if log_scale:
         ax = plt.gca()
         ax.set_yscale("log")
@@ -62,16 +65,23 @@ def PRCurve(gt_labels, pred_scores, log_scale=True, norm=False, savename=None):
         gt_scores = np.array(list(gt_labels.values()))
         predictions = np.array(list(predictions.values()))
         
-
+    # Get precision, recall and thresholds
     p, r, ts = prcurve(gt_scores, predictions, pos_label=1.0)
     
+    # Get the area-under-curve for PR (PR-AUC)
     pr_auc = auc(r, p)
 
+    # Initialize plot figure size and style
+    plt.figure(figsize=figsize)
+    set_plot_style()
+
+    # Plot the results
     plt.plot(r, p)
     plt.xlabel('Recall')
     plt.ylabel('Precision')
     plt.title(f'Precision-Recall Curve (AUC: {round(pr_auc, 2)})')
 
+    # Save plot if specified
     if savename is not None:
         plt.savefig(f'{savename}.png')
         plt.close()
@@ -85,21 +95,28 @@ def PRCombine(ps, rs, aucs, labels=['Random', 'Rule-based'], log_scale=True, sav
     Combines the precision and recall curves for multiple methods, plots labels and AUCs
     """
     plt.clf()
+
+    # Plot log scale if specified
     if log_scale:
         ax = plt.gca()
         ax.set_yscale("log")
     
+    # Plot the individual precision and recalls for each method
     for p, r in zip(ps, rs):
         plt.plot(r, p)
+    
+    # Initialize plot figure size and style
+    plt.figure(figsize=figsize)
+    set_plot_style()
         
+    # Plot the results
     labels = [f'{label} (auc: {round(auroc, 2)})' for label, auroc in list(zip(labels, aucs))]
-
     plt.legend(labels)
-        
     plt.xlabel('Recall')
     plt.ylabel('Precision')
     plt.title('Precision-Recall Curve')
 
+    # Save plot if specified
     if savename is not None:
         plt.savefig(f'{savename}.png')
         plt.close()
@@ -126,10 +143,16 @@ def read_fscores_experiment(folder):
     Reads the fscores from an experiment run using the "exp_all_methods.py" script, which are the three main experiments,
     namely sparsity, tfreq and noise experiments. It reads the folders and collects the fscores from each experiment for all methods
     """
+
+    # Sort the folder in correct order of results
     folders = [f for f in natsorted(os.listdir(folder)) if os.path.isdir(os.path.join(folder,f))]
+
+    # Make sure "nonoise" folder is first in the folders list
     nonoise_check = [True for f in folders if 'nonoise' in f]
     if len(nonoise_check) == 1 and nonoise_check[0]:
         folders = [folders[-1], *folders[0:-1]]
+
+    # Get results for each folder
     results = []
     for f in folders:
         results_folder = os.path.join(folder, f)
@@ -141,6 +164,7 @@ def read_fscores_experiment(folder):
             result = pickle.load(handle)
         results.append(result)
     
+    # Get fscores for each folder
     fscores_random = []
     fscores_rulebased = []
     fscores_hist = []
@@ -150,6 +174,8 @@ def read_fscores_experiment(folder):
         fscores_rulebased.append(result['results']['rulebased']['fscore'])
         fscores_hist.append(result['results']['histogram']['fscore'])
         fscores_hmm.append(result['results']['hmm']['fscore'])
+
+    # Return fscores
     return fscores_random, fscores_rulebased, fscores_hist, fscores_hmm
 
 def read_prauc_experiment(folder):
@@ -157,10 +183,16 @@ def read_prauc_experiment(folder):
     Reads the PR-AUC from an experiment run using the "exp_all_methods.py" script, which are the three main experiments,
     namely sparsity, tfreq and noise experiments. It reads the folders and collects the PR-AUCs from each experiment for all methods
     """
+
+    # Sort the folder in correct order of results
     folders = [f for f in natsorted(os.listdir(folder)) if os.path.isdir(os.path.join(folder,f))]
+
+    # Make sure "nonoise" folder is first in the folders list
     nonoise_check = [True for f in folders if 'nonoise' in f]
     if len(nonoise_check) == 1 and nonoise_check[0]:
         folders = [folders[-1], *folders[0:-1]]
+
+    # Get results for each folder
     results = []
     for f in folders:
         results_folder = os.path.join(folder, f)
@@ -172,6 +204,8 @@ def read_prauc_experiment(folder):
             result = pickle.load(handle)
         results.append(result)
     
+    
+    # Get PR-AUCs for each folder
     prauc_random = []
     prauc_rulebased = []
     prauc_hist = []
@@ -182,12 +216,19 @@ def read_prauc_experiment(folder):
         prauc_hist.append(result['results']['histogram']['pr_auc'])
         prauc_hmm.append(result['results']['hmm']['pr_auc'])
 
+    # Return PR-AUCs
     return prauc_random, prauc_rulebased, prauc_hist, prauc_hmm
 
-def Ch_vs_y(fscores, Chs, y='F-Score', savename=None, figsize=(10,7)):
+def Ch_vs_y(fscores, Chs, y='F-Score', savename=None, figsize=(8,6)):
     """
     Plots the Ch (heading parameter) vs some custom variable y for the Rule-based method ablation experiments
     """
+    
+    # Initialize plot figure size and style
+    plt.figure(figsize=figsize)
+    set_plot_style()
+
+    # Plot results
     plt.figure(figsize=figsize)
     plt.plot(Chs, fscores, '-o')
     plt.ylabel(y)
@@ -195,6 +236,7 @@ def Ch_vs_y(fscores, Chs, y='F-Score', savename=None, figsize=(10,7)):
     plt.title(f'Heading Weight vs {y}')
     plt.ylim(0, 1)
     
+    # Save figure if specified
     if savename is not None:
         plt.savefig(f'{savename}.png')
         plt.close()
@@ -205,6 +247,12 @@ def dim_vs_y(fscores, dims, y='F-Score', savename=None, figsize=(10,7)):
     """
     Plots the dim/resolution of the histogram vs some custom variable y for the histogram method ablation experiments
     """
+    
+    # Initialize plot figure size and style
+    plt.figure(figsize=figsize)
+    set_plot_style()
+
+    # Plot results
     plt.figure(figsize=figsize)
     plt.plot(dims, fscores, '-o')
     plt.ylabel(y)
@@ -212,6 +260,7 @@ def dim_vs_y(fscores, dims, y='F-Score', savename=None, figsize=(10,7)):
     plt.title(f'Histogram Dimension vs {y}')
     plt.ylim(0, 1)
     
+    # Save results if specified
     if savename is not None:
         plt.savefig(f'{savename}.png')
         plt.close()
@@ -222,14 +271,21 @@ def x_vs_fscore(x, labels, xlabel=None, folder='./dummy_results/', savename=None
     """
     Plots some variable vs fscore, by reading results from the given folder
     """
-
+    
+    # Read the results from the folders
     fscores_random, fscores_rulebased, fscores_hist, fscores_hmm = read_fscores_experiment(folder)
     
+    # Initialize plot figure size and style
     plt.figure(figsize=figsize)
+    set_plot_style()
+
+    # Plot the 4 different methods
     plt.plot(labels, fscores_random, '-o')
     plt.plot(labels, fscores_rulebased, '-o')
     plt.plot(labels, fscores_hist, '-o')
     plt.plot(labels, fscores_hmm, '-o')
+    
+    # Set axes labels, title, legend and y limit
     plt.ylabel('F-Score')
     if xlabel is not None:
         plt.xlabel(xlabel)
@@ -237,24 +293,55 @@ def x_vs_fscore(x, labels, xlabel=None, folder='./dummy_results/', savename=None
     plt.legend(['Random', 'Rule-based', 'Histogram', 'HMM'])
     plt.ylim(0, 1)
     
+    # Save figure if specified
     if savename is not None:
         plt.savefig(f'{savename}.png')
         plt.close()
     else:
         plt.show()
 
+def set_plot_style():
+    # use a gray background
+    ax = plt.axes(facecolor='#E6E6E6')
+    ax.set_axisbelow(True)
+
+    # draw solid white grid lines
+    plt.grid(color='w', linestyle='solid')
+
+    # hide axis spines
+    for spine in ax.spines.values():
+        spine.set_visible(False)
+        
+    # hide top and right ticks
+    ax.xaxis.tick_bottom()
+    ax.yaxis.tick_left()
+
+    # lighten ticks and labels
+    ax.tick_params(colors='gray', direction='out')
+    for tick in ax.get_xticklabels():
+        tick.set_color('gray')
+    for tick in ax.get_yticklabels():
+        tick.set_color('gray')
+
 def x_vs_prauc(x, labels, xlabel=None, folder='./dummy_results/', savename=None, figsize=(10,7)):
     """
     Plots some variable vs precision and recall AUC given folder containing results over multiple noise configurations
     """
-
+    
+    # Read the results from the folders
     prauc_random, prauc_rulebased, prauc_hist, prauc_hmm = read_prauc_experiment(folder)
     
+    # Initialize plot figure size and style
     plt.figure(figsize=figsize)
+    set_plot_style()
+    
+    # Plot the 4 different methods
     plt.plot(labels, prauc_random, '-o')
     plt.plot(labels, prauc_rulebased, '-o')
     plt.plot(labels, prauc_hist, '-o')
     plt.plot(labels, prauc_hmm, '-o')
+
+    # Set axes labels, title, legend and y limit
     plt.ylabel('PR-AUC')
     if xlabel is not None:
         plt.xlabel(xlabel)
@@ -262,22 +349,31 @@ def x_vs_prauc(x, labels, xlabel=None, folder='./dummy_results/', savename=None,
     plt.legend(['Random', 'Rule-based', 'Histogram', 'HMM'])
     plt.ylim(0, 1)
     
+    # Save figure if specified
     if savename is not None:
         plt.savefig(f'{savename}.png')
         plt.close()
     else:
         plt.show()
 
-def bar_fscore(fscores, labels, savename=None):
+def bar_fscore(fscores, labels, savename=None, figsize=(8,6)):
     """
     Plots a sequence of F-Scores in a bar chart for comparing histogram configurations
     """
+    
+    # Initialize plot figure size and style
+    plt.figure(figsize=figsize)
+    set_plot_style()
+    
+    # Plot results
     plt.bar(labels, fscores)
     plt.ylabel('F-Score')
     plt.title('Histogram Configuration vs F-Score')
     plt.ylim(0, 1)
     for i in range(len(labels)):
         plt.text(i-0.1,round(fscores[i],2),round(fscores[i],2))
+
+    # Save results if specified
     if savename is not None:
         plt.savefig(f'{savename}.png')
         plt.close()
@@ -336,94 +432,25 @@ def compare_experiments_prauc(folders=['results_high_sample', 'results_high_samp
     else:
         plt.show()
 
-def recalc_final_curves(folder):
-    steps, step_size = 6, 3
+def replot_tfreq_results(folder, steps=6, step_size=3):
     resample_traj_steps = np.arange(1,(steps*step_size)+1, step_size)
-    savedir = os.path.join('./experimental_results', folder)
+
+    exp_folder = os.path.join('./experimental_results', folder)
+    if not os.path.exists(exp_folder):
+        raise ValueError(f'Folder {exp_folder} not found')
     # Save plots
     x_vs_fscore(x='Trace Frequency', labels=resample_traj_steps, xlabel='Trace Frequency',
-                folder=savedir, 
-                savename=os.path.join(savedir, 'tfreq_vs_fscore'))
+                folder=exp_folder,
+                savename=os.path.join(exp_folder, "tfreq_vs_fscore"), figsize=(8,6))
     plt.close()
     x_vs_prauc(x='Trace Frequency', labels=resample_traj_steps, xlabel='Trace Frequency',
-                folder=savedir, 
-                savename=os.path.join(savedir, 'tfreq_vs_prauc'))
+                folder=exp_folder,
+                savename=os.path.join(exp_folder, "tfreq_vs_prauc"), figsize=(8,6))
 
-    # # Save GIF of how the trajectories change
-    # save_gif(folder=f'./experimental_results/results_{args.exp_name}_seed{args.seed}/',
-    #             img_name='G1T2', savename=f'./experimental_results/results_{args.exp_name}_seed{args.seed}/G1T2')
-
-# def recalc_pr_scores(folder):
-#     pbar = tqdm([os.path.join(folder, f) for f in os.listdir(folder) if os.path.isdir(os.path.join(folder, f))])
-#     for sf in pbar:
-#         pbar.set_description(f'Recalulating results for folder: {sf}')
-#         result_files = [os.path.join(sf,f) for f in os.listdir(sf) if 'hdf5' in f]
-#         if len(result_files) != 1:
-#             raise ValueError(f"Expected one results file (hdf5) in {sf}, but found {len(result_files)}: {result_files}")
-#         res_fname = result_files[0]
-#         with open(res_fname, 'rb') as handle:
-#             result = pickle.load(handle)
-
-#         # Read arguments from results and setup dataset
-#         noise, noise_config, map_index, bbox = result['args']['noise'], result['args']['noise_config'], \
-#             result['args']['map_index'], result['args']['bbox']
-#         dataset = SHDataset(noise=noise, noise_config=noise_config)
-#         G1,_,G2,_ = dataset.read_snapshots(map_index, bbox=bbox)
-#         gt_labels = groundtruth_labels(G1, G2)
-
-#         # Read scores for random method, recompute precision recall, overwrite old results
-#         scores_rand = result['results']['random']['scores']
-#         ps_rand, rs_rand, ts_rand, prauc_rand = PRCurve(gt_labels, scores_rand, log_scale=False, savename=os.path.join(sf, 'prcurve_random'))
-#         PRCurve(gt_labels, scores_rand, log_scale=True, savename=os.path.join(sf, 'prcurve_logscale_random'))
-#         result['results']['random']['precision'] = ps_rand
-#         result['results']['random']['recall'] = rs_rand
-#         result['results']['random']['thresholds'] = ts_rand
-#         result['results']['random']['pr_auc'] = prauc_rand
-
-#         # Read scores for rule-based method, recompute precision recall, overwrite old results
-#         scores_rb = result['results']['rulebased']['scores']
-#         ps_rb, rs_rb, ts_rb, prauc_rb = PRCurve(gt_labels, scores_rb, log_scale=False, savename=os.path.join(sf, 'prcurve_rulebased'))
-#         PRCurve(gt_labels, scores_rb, log_scale=True, savename=os.path.join(sf, 'prcurve_logscale_rulebased'))
-#         result['results']['rulebased']['precision'] = ps_rb
-#         result['results']['rulebased']['recall'] = rs_rb
-#         result['results']['rulebased']['thresholds'] = ts_rb
-#         result['results']['rulebased']['pr_auc'] = prauc_rb
-
-#         # Read scores for HMM method, recompute precision recall, overwrite old results
-#         scores_hmm = result['results']['hmm']['scores']
-#         ps_hmm, rs_hmm, ts_hmm, prauc_hmm = PRCurve(gt_labels, scores_hmm, log_scale=False, savename=os.path.join(sf, 'prcurve_hmm'))
-#         PRCurve(gt_labels, scores_hmm, log_scale=True, savename=os.path.join(sf, 'prcurve_logscale_hmm'))
-#         result['results']['hmm']['precision'] = ps_hmm
-#         result['results']['hmm']['recall'] = rs_hmm
-#         result['results']['hmm']['thresholds'] = ts_hmm
-#         result['results']['hmm']['pr_auc'] = prauc_hmm
-
-#         # Overwrite combined PR curves
-#         PRCombine(ps=[ps_rand, ps_rb, ps_hmm], rs=[rs_rand, rs_rb, rs_hmm], aucs=[prauc_rand, prauc_rb, prauc_hmm]\
-#                     ,labels=['Random', 'Rule-based', 'HMM'], log_scale=False, savename=os.path.join(sf, 'prcurve_combined'))
-#         PRCombine(ps=[ps_rand, ps_rb, ps_hmm], rs=[rs_rand, rs_rb, rs_hmm], aucs=[prauc_rand, prauc_rb, prauc_hmm]\
-#                     ,labels=['Random', 'Rule-based', 'HMM'], log_scale=True, savename=os.path.join(sf, 'prcurve_logscale_combined'))
-
-#         # Resave pickled results
-#         with open(res_fname, 'wb') as handle:
-#             pickle.dump(result, handle, protocol=pickle.HIGHEST_PROTOCOL)
-
-#     plt.close()
-#     fscore_vs_noise(folder=folder, savename=os.path.join(folder, 'fscore_vs_noise'))
-#     plt.close()
-#     prauc_vs_noise(folder=folder, savename=os.path.join(folder, 'prauc_vs_noise'))
-#     plt.close()
-
-        
-# recalc_final_curves(folder='results_tfreq_exp_moresteps_seed42')
-# recalc_pr_scores(folder='./experimental_results/results_tfreq_exp_moresteps_seed42')
-# recalc_pr_scores(folder='results_high_sample_10000')
-# recalc_pr_scores(folder='results_high_sample_v3')
-# recalc_pr_scores(folder='results_high_sample')
-# recalc_pr_scores(folder='results_high_sample_coveragepatch')
-# recalc_pr_scores(folder='results_low_sample_coveragepatch')
-
-
-# compare_experiments()
-# compare_experiments_fscore(folders=[ 'results_high_sample_10000_tuned', 'results_high_sample_10000'], savename='fscore_tuned_vs_untuned')
-# compare_experiments_prauc(folders=['results_high_sample_10000', 'results_high_sample_10000_tuned'], savename='prauc_tuned_vs_untuned')
+def replot_tfreq_exps()
+    folders = [f for f in os.listdir('experimental_results') if 'tfreq' in f]
+    for f in folders:
+        if 'moresteps' in f:
+            replot_results(f, steps=6, step_size=3)
+        else:
+            replot_results(f, steps=5, step_size=1)
