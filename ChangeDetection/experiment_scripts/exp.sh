53b96aeed113e367cf0982d0a97a4aa2b891c244
Kevin Waller <waller@deepdriver03.flatns.net>
2022-05-25 14:48:03 +0200

    updated exp.sh script to include extra experiments, updated a notebook

--- ChangeDetection/experiment_scripts/exp.sh
+++ ChangeDetection/experiment_scripts/exp.sh
@@ -1,11 +1,15 @@
 
 # Run all experiments on seed 42
-python -m experiment_scripts.noise_experiment --exp_name noise_exp --n_traj 0 --num_cpu_hmm 64 --seed 42;
-python -m experiment_scripts.sparsity_experiment --exp_name sparsity_exp_max10000_wnoise --max_trajectories 10000 --noise --noise_config 1 --num_cpu_hmm 64 --seed 42;
-python -m experiment_scripts.tfreq_experiment --exp_name tfreq_exp_wnoise --n_traj 0 --noise --noise_config 1 --num_cpu_hmm 64 --seed 42;
-python -m experiment_scripts.histogram_experiment --exp_name hist_exp --seed 42;
-python -m experiment_scripts.histogram_resolution_experiment --exp_name histres_exp --seed 42;
-python -m experiment_scripts.rulebased_experiment --exp_name rulebased_exp --seed 42;
+# python -m experiment_scripts.noise_experiment --exp_name noise_exp --n_traj 0 --num_cpu_hmm 64 --seed 42;
+# python -m experiment_scripts.sparsity_experiment --exp_name sparsity_exp_max10000_wnoise --max_trajectories 10000 --noise --noise_config 1 --num_cpu_hmm 64 --seed 42;
+# python -m experiment_scripts.tfreq_experiment --exp_name tfreq_exp_wnoise --n_traj 0 --noise --noise_config 1 --num_cpu_hmm 64 --seed 42;
+# python -m experiment_scripts.histogram_experiment --exp_name hist_exp --seed 42;
+# python -m experiment_scripts.histogram_resolution_experiment --exp_name histres_exp --seed 42;
+# python -m experiment_scripts.rulebased_experiment --exp_name rulebased_exp --seed 42;
+# python -m experiment_scripts.sparsity_experiment --exp_name sparsity_exp_wnoise --noise --noise_config 1 --num_cpu_hmm 64 --seed 42;
+# python -m experiment_scripts.sparsity_experiment --exp_name sparsity_exp --num_cpu_hmm 64 --seed 42;
+python -m experiment_scripts.hmm_analysis --exp_name hmm_analysis_exp --n_traj 0 --num_cpu_hmm 64 --seed 42;
+python -m experiment_scripts.sparsity_experiment --exp_name sparsity_exp_max10000 --max_trajectories 10000 --num_cpu_hmm 64 --seed 42;
 
 # Run all experiments on seed 142
 # python -m experiment_scripts.noise_experiment --exp_name noise_exp --n_traj 0 --num_cpu_hmm 64 --seed 142;
